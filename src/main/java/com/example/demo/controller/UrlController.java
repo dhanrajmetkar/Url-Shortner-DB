17e846862200d1778d319502cbc4f1090f3a6e00
dhanrjmetkar <dhanrajmetkar3660@gmail.com>
2024-07-05 16:28:53 +0530

    formated project structure

--- src/main/java/com/example/demo/controller/UrlController.java
+++ src/main/java/com/example/demo/controller/UrlController.java
@@ -1,45 +1,40 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Domain;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
-
 import com.example.demo.entity.Url;
-import com.example.demo.service.UrlServiceImpl;
 import com.example.demo.service.UrlService;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
-import javax.management.openmbean.CompositeData;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 @RestController
 @RequestMapping()
 public class UrlController {
 
     @Autowired
     private UrlService urlService;
-   @Autowired
-    private UrlServiceImpl urlServiceimpl;
+
     RedirectView redirectView = new RedirectView();
+
     @GetMapping(path="/allurl")
     public @ResponseBody Iterable<Url> getAll() {
-        return urlServiceimpl.getAllUrl();
+        return urlService.getAllUrl();
     }
+
     @GetMapping(path="/alld")
     public @ResponseBody Iterable<Domain> getd() {
-        return urlServiceimpl.getAllDomain();
+        return urlService.getAllDomain();
     }
+
     @GetMapping("/getCount")
     public ResponseEntity<Object> getcount()
     {
-        List<Map.Entry<String, Integer>>list =urlServiceimpl.getcount();
+        List<Map.Entry<String, Integer>> list = urlService.getcount();
         if(list.isEmpty())
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         return
@@ -47,48 +42,20 @@ public class UrlController {
 
     }
     @GetMapping("/{key}")
-    public RedirectView getUrl(@PathVariable("key") String key)
-    {
-        Url temp= urlServiceimpl.getUrl(key);
-        String url= temp.getUrl();
-        RedirectView redirectView=new RedirectView();
+    public RedirectView getUrl(@PathVariable("key") String key) {
+        Url temp = urlService.getUrl(key);
+        String url = temp.getUrl();
+        RedirectView redirectView = new RedirectView();
         redirectView.setUrl(url);
         return redirectView;
     }
     @PostMapping("/shortUrl")
         public String shorturl(@RequestBody String url)
         {
-            Url temp=this.urlServiceimpl.addUrl(url);
+            Url temp = this.urlService.addUrl(url);
             String key="http://localhost:8080/"+temp.getShorturl();
             redirectView.setUrl(key);
-//            URL ur;
-//            try {
-//                ur = new URL(url);
-//            } catch (MalformedURLException e) {
-//                throw new RuntimeException(e);
-//            }
-//            String domain=ur.getHost();
-//            Domain domainobj=new Domain();
-//            int id;
-//            boolean flag=true;
-//            List<Domain> domains = urlServiceimpl.getAllDomain();
-//            if(domains.size()>0) {
-//                for (Domain domain1 : domains) {
-//                    String d1 = domain1.getDomainName();
-//                    if (domain.equals(d1)) {
-//                        int n = domain1.getCount();
-//                        domain1.setCount(n + 1);
-//                        flag=false;
-//                        urlServiceimpl.addDomainDb(domain1);
-//                    }
-//                }
-//            }
-//            domainobj.setDomainName(domain);
-//            domainobj.setCount(1);
-//
-              urlService.addUrlDb(temp);
-//            if(flag)
-//            urlServiceimpl.addDomainDb(domainobj);
+            urlService.addUrlDb(temp);
             return key;
         }
 }
--- src/main/java/com/example/demo/service/UrlService.java
+++ src/main/java/com/example/demo/service/UrlService.java
@@ -1,7 +1,21 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Domain;
 import com.example.demo.entity.Url;
+
+import java.util.List;
+import java.util.Map;
 
 public interface UrlService {
     public Url addUrlDb(Url url);
+
+    Iterable<Url> getAllUrl();
+
+    Iterable<Domain> getAllDomain();
+
+    List<Map.Entry<String, Integer>> getcount();
+
+    Url getUrl(String key);
+
+    Url addUrl(String url);
 }
