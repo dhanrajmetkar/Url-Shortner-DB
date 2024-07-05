f59204264f889dc23ce11e377bd59235d4cc962d
dhanrjmetkar <dhanrajmetkar3660@gmail.com>
2024-07-05 16:38:14 +0530

    lombok code

--- src/main/java/com/example/demo/entity/Domain.java
+++ src/main/java/com/example/demo/entity/Domain.java
@@ -4,28 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Domain {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     Integer id;
     String domainName;
     Integer count;
-
-    public String getDomainName() {
-        return domainName;
-    }
-
-    public void setDomainName(String domainName) {
-        this.domainName = domainName;
-    }
-
-    public Integer getCount() {
-        return count;
-    }
-
-    public void setCount(Integer count) {
-        this.count = count;
-    }
 }
